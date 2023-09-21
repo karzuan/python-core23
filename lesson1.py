2b152cfa87642ed5de6990db632914b2c7d5b221
Konstantin Kuts <karzuan@gmail.com>
2023-09-21 11:02:35 +0500

    first lesson commit.

---
+++ lesson1.py
@@ -0,0 +1,21 @@
+a = 10
+def foo():
+    pass
+
+class Bar:
+    pass
+
+# print(type(a))
+# print(type(foo))
+# print(type(Bar))
+#
+#
+
+b = 1
+print(id(b))
+
+b += 1
+print(id(b))
+
+b -= 1
+print(id(b))
